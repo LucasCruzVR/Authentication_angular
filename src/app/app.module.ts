73ab4d3be6a510573d6995b738570229a5cfd2e5
lucas cruz <lucas_cruz100@live.com>
2023-01-29 13:13:01 -0300

    style for authenticated page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +18,7 @@ import { AuthenticatedComponent } from './components/authenticated/authenticated
     AuthenticatedComponent
   ],
   imports: [
+    MatIconModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
